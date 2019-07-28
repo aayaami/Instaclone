96058580dea40a202f2f56a196f4c33608696c59
Aya <vostroknutov.alexander@gmail.com>
2019-07-28 17:33:38 +0400

    Prepare for deployment 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 require('dotenv').config()
 
@@ -21,6 +22,16 @@ app.use('/api/users/', require('./routes/api/users'))
 app.use('/api/auth/', require('./routes/api/auth'))
 app.use('/api/posts/', require('./routes/api/posts'))
 app.use('/api/chats/', require('./routes/api/chats'))
+
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.statis('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = 5000
 
